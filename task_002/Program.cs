8864f502e108ec5ea9d74439ddd22ecae8964a8e
Anastasia <nastyv15@mail.ru>
2023-01-30 23:47:36 +0300

    add task_002

--- task_002/Program.cs
+++ task_002/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+Console.WriteLine("Введите число A:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число C:");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+
+if (c > max)
+{
+    max = c;
+}
+
+Console.WriteLine("Наибольшее из введённых чисел -> " + max);
+
+/*if (a > b) 
+{
+    int a = max;
+    Console.WriteLine($"Максимальное число : {max} = {a}");
+}
+else 
+{
+   Console.WriteLine($"Максимальное число : {max} = {b}"); 
+}
+if (b > c)
+{
+    int b =  max;
+    Console.WriteLine($"Максимальное число : {max} = {b}");
+}
+else 
+{
+    Console.WriteLine($"Максимальное число : {max} = {c}");
+}
+if (c > a)
+{
+    int c = max;
+    Console.WriteLine($"Максимальное число : {max} = {c}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число : {max} = {a}");
+}
+*/    
+
+
+
