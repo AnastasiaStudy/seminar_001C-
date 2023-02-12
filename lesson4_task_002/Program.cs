0423270cab0e41ef0ee030ec97f4cfd633652457
Anastasia <nastyv15@mail.ru>
2023-02-12 20:18:35 +0300

    add lesson4_tasks

--- lesson4_task_002/Program.cs
+++ lesson4_task_002/Program.cs
@@ -4,3 +4,33 @@
 82 -> 10
 9012 -> 12
 */
+
+
+int GetNumber(string message)
+{
+    System.Console.Write(message);
+    string readInput = System.Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+
+
+int GetSumOfDigits(int number)
+{
+    int result = 0;
+    while(number > 0)
+    {
+        result += number % 10;
+        number = number / 10;
+    }
+    return result;
+}
+
+int number = GetNumber("Введите число: ");
+Console.WriteLine($"Сумма цифр в числе {number} = {GetSumOfDigits(number)}");
+
+
+
+
+
+
--- lesson4_task_003/Program.cs
+++ lesson4_task_003/Program.cs
@@ -3,3 +3,39 @@
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33]
 */
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string ReadInput = System.Console.ReadLine();
+    int result = int.Parse(ReadInput);
+    return result;
+}
+
+int[] GenerateArray(int Lenght, int minValue, int maxValue)
+{
+    int[] array = new int[Lenght];
+    Random random = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = random.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+    }
+    System.Console.Write($"{array[array.Lenght - 1]}");
+    System.Console.WriteLine("]");
+}
+
+int length = Prompt("Длина массива: ");
+int min = Prompt("Начальное значение: ");
+int max = Prompt("Конечное значение: ");
+int[] array = GenerateArray(Lenght, min, max);
+PrintArray(array);
